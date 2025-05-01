9d8cb8e9d9ecc186e791e67809bf9c1c4b985165
LG <luizgustavovargas28@gmail.com>
2025-05-01 11:59:44 -0300

    feat: Add archive .gitignore and configure .env in config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
+import 'dotenv/config'
+
 export default {
-  connectionString: '', // To test, remove before
+  connectionString: process.env.DATABASE_URL, // To test, remove before
   threshold: 20,                                  // % dead tuples
   minSize: '1MB',                                // minSize to be a fragmented table
   minSizeBytes: 1048576,                         // minSize = '1MB' in bytes
