b0d3483abe6721726fe415880b7992c871426c37
Kelvin <kelvingremista2015@hotmail.com>
2025-05-03 03:36:14 -0300

    added more tests and define threshold default 20

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 export default {
-  threshold: 0,                           // % dead tuples
+  threshold: 20,                           // % dead tuples
   minSizeBytes: 1024 * 8,                  // 8 KB, tamanho mínimo em bytes
   useFullVacuum: false,                    // Vacuum completo (opcional)
   // connectionString: process.env.DATABASE_URL, // Remover, pois agora é fornecido pelo usuário via CLI
--- tests/unit/TableAnalyzer.test.js
+++ tests/unit/TableAnalyzer.test.js
@@ -31,10 +31,44 @@ describe('TableAnalyzer Unit Tests', () => {
       table_size: "1 MB"
     },
   ];
+
   test('should identify fragmented tables', () => {
     const result = TableAnalyzer.analyze(mockStats, mockConfig);
     expect(result).toHaveLength(2);
     expect(result[0].table_name).toBe('users');
     expect(result[1].table_name).toBe('sellers');
+  });
+
+  test('should skip small table if is fragmented', () => {
+    const mockWithSmallTable = [...mockStats, {
+      schemaname: 'public',
+      relname: 'vendors',
+      n_live_tup: 500,
+      dead_tup_percentage: 48.447,
+      n_dead_tup: 1000,
+      table_size: "100 kB"
+    }]
+
+    const result = TableAnalyzer.analyze(mockWithSmallTable, mockConfig);
+    expect(result).toHaveLength(2);
+    expect(result[0].table_name).toBe('users');
+    expect(result[1].table_name).toBe('sellers');
+  });
+
+  test('should include big table if is fragmented', () => {
+    const mockWithBigTable = [...mockStats, {
+      schemaname: 'public',
+      relname: 'teste',
+      n_live_tup: 18756,
+      dead_tup_percentage: 88.447,
+      n_dead_tup: 15786,
+      table_size: "40 GB"
+    }]
+
+    const result = TableAnalyzer.analyze(mockWithBigTable, mockConfig);
+    expect(result).toHaveLength(3);
+    expect(result[0].table_name).toBe('users');
+    expect(result[1].table_name).toBe('sellers');
+    expect(result[2].table_name).toBe('teste');
   });
 });
